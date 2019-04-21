15a0d47cb559956d8946726f55abe2b6c4cc81a2
Miłosz Piechocki <miloszpp@gmail.com>
2019-04-21 22:10:56 +0200

    implementation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import './../barecss/css/bare.min.css';
 
+import { Observable, merge, Subject, fromEvent, empty } from 'rxjs';
+import { publish, skipWhile, takeWhile, take, tap, map, switchMap, repeatWhen, delay, takeUntil, share, withLatestFrom } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
 interface AnalyzeTask {
   id: number;
   status: 'inProgress' | 'cancelled' | 'finished';
@@ -27,3 +31,47 @@ const updateUI = (task: AnalyzeTask) => {
   }
 }
 
+const takeWhileInclusive =
+  <T>(predicate: (item: T) => boolean) => (stream$: Observable<T>) =>
+    stream$.pipe(
+      publish(shared$ => merge(
+        shared$.pipe(takeWhile(predicate)),
+        shared$.pipe(skipWhile(predicate), take(1)),
+      ))
+    );
+
+const cancelSubject = new Subject();
+
+const result$ = fromEvent(analyzeButtonEl, 'click').pipe(
+  tap(() => cancelSubject.next()),
+  map(() => inputEl.value),
+  switchMap((text) => ajax.post(`${url}/analyze`, { message: text })),
+  map((ajaxResponse) => ajaxResponse.response as AnalyzeTask),
+  switchMap((task) => ajax.getJSON<AnalyzeTask>(
+    `${url}/analyze/${task.id}`).pipe(
+      repeatWhen(delay(1000)), 
+      takeWhileInclusive((response) => response.status === 'inProgress'),
+      takeUntil(cancelSubject),
+    ),
+  ),
+  share(),
+);
+
+result$.subscribe(updateUI);
+
+cancelSubject.pipe(
+  withLatestFrom(result$),
+  switchMap(([, result]) => 
+    result.status === 'inProgress'
+      ? ajax.post(`${url}/analyze/${result.id}/cancel`)
+      : empty()
+  ),
+).subscribe(
+  () => {
+    resultsSectionEl.innerHTML = '';
+  }
+);
+
+fromEvent(cancelButtonEl, 'click').subscribe(
+  () => cancelSubject.next()
+);
