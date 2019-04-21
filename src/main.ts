90907d68531d50b9904cb6b9f488593ff39f8696
Miłosz Piechocki <miloszpp@gmail.com>
2019-04-21 21:58:46 +0200

    groundwork

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
-class Car {
-    constructor(private engine: string) {
+import './../barecss/css/bare.min.css';
 
-    }
+interface AnalyzeTask {
+  id: number;
+  status: 'inProgress' | 'cancelled' | 'finished';
+  result?: 'positive' | 'negative';
 }
 
-setInterval(() => {
-    document.getElementsByTagName("h1")[0].innerText = "Bye!";
-}, 1000);
+const url = 'http://localhost:3000';
+
+const inputEl = document.getElementById('input-text') as HTMLTextAreaElement;
+const resultsSectionEl = document.getElementById('section-results') as HTMLDivElement;
+const analyzeButtonEl = document.getElementById('button-analyze') as HTMLButtonElement;
+const cancelButtonEl = document.getElementById('button-cancel') as HTMLButtonElement;
+
+const updateUI = (task: AnalyzeTask) => {
+  switch (task.status) {
+    case 'inProgress':
+      resultsSectionEl.innerHTML = 'Loading...';
+      return;
+    case 'finished':
+      resultsSectionEl.innerHTML = `Result: ${task.result === 'positive' ? '😀' : '☹️'}`;
+      return;
+    case 'cancelled':
+      resultsSectionEl.innerHTML = '';
+      return;
+  }
+}
+
