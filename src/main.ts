35fde8e2ceb14e091466cc932a9a70409fdca154
Miłosz Piechocki <miloszpp@gmail.com>
2019-04-24 23:18:02 +0200

    exponential backoff and caching

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './../barecss/css/bare.min.css';
 
 import { Observable, merge, Subject, fromEvent, empty, of, throwError, timer } from 'rxjs';
-import { publish, skipWhile, takeWhile, take, tap, map, switchMap, repeatWhen, delay, takeUntil, share, withLatestFrom, retry, retryWhen, filter, throwIfEmpty, delayWhen } from 'rxjs/operators';
+import { publish, skipWhile, takeWhile, take, tap, map, switchMap, repeatWhen, delay, takeUntil, share, withLatestFrom, retry, retryWhen, filter, throwIfEmpty, delayWhen, shareReplay } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
+import {memoize} from 'lodash';
 
 interface AnalyzeTask {
   id: number;
@@ -109,3 +110,43 @@ fromEvent(getPriceButtonEl, 'click').pipe(
 );
 
 //--------
+
+interface Article {
+  id: number;
+  title: string;
+}
+
+type ArticleWithBody = Article & { body: string };
+
+const getArticlesButtonEl = document.getElementById('button-get-articles')!;
+const articlesListEl = document.getElementById('list-articles')!;
+const bodyParagraphEl = document.getElementById('paragraph-body')!;
+
+const article$ = ajax.getJSON<Article[]>(`${url}/articles`).pipe(
+  shareReplay(1)
+);
+
+fromEvent(getArticlesButtonEl, 'click').pipe(
+  switchMap(() => article$),
+  retry(),
+).subscribe(articles => {
+  articlesListEl.innerHTML = articles.map(
+    article => `<option value='${article.id}'>${article.title}</option>`
+  ).join('');
+});
+
+const getArticle$ = memoize((articleId: string) => 
+  ajax.getJSON<ArticleWithBody>(`${url}/articles/${articleId}`).pipe(
+    shareReplay(1)
+  )
+);
+
+fromEvent(articlesListEl, 'change').pipe(
+  switchMap(event => {
+    const articleId = (event.target as HTMLSelectElement).value;
+    return getArticle$(articleId);
+  }),
+  retry(),
+).subscribe(article => {
+  bodyParagraphEl.innerText = article.body;
+});
